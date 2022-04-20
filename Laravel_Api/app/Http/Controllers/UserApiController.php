1cffd2efb88d3eec0632ba2094c06f78ac09d508
Md Nahidul Islam <nahidulislamuiu@gmail.com>
2022-04-20 23:30:58 +0600

    addUser

--- Laravel_Api/app/Http/Controllers/UserApiController.php
+++ Laravel_Api/app/Http/Controllers/UserApiController.php
@@ -25,22 +25,22 @@ class UserApiController extends Controller
 
         if ($request->isMethod('post')) {
             $data = $request->all();
-            
-        $rules = [
-            'name' => 'required',
-            'email' => 'required|email',
-            'password' => 'required',
-        ];
-    
-        $customMessages = [
-            'name.required' => 'Name field is required.',
-            'email.email' => 'Email field is required.',
-            'password.required' => ' Password  field is required.'
-        ];
-    
-        $validator=Validator::make($data, $rules, $customMessages);
-            if($validator->fails()){
-                return response()->json($validator->errors(),422);
+
+            $rules = [
+                'name' => 'required',
+                'email' => 'required|email',
+                'password' => 'required',
+            ];
+
+            $customMessages = [
+                'name.required' => 'Name field is required.',
+                'email.email' => 'Email field is required.',
+                'password.required' => ' Password  field is required.'
+            ];
+
+            $validator = Validator::make($data, $rules, $customMessages);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
             }
 
             $user = new User;
@@ -50,6 +50,41 @@ class UserApiController extends Controller
             $user->save();
             $message = 'user successfully Added';
             return response()->json(['message' => $message], 201);
+        }
+    }
+
+    public function AddMultipleUser(Request $request)
+    {
+
+
+        if ($request->isMethod('post')) {
+            $data = $request->all();
+
+            $rules = [
+                'users.*.name' => 'required',
+                'users.*.email' => 'required|email',
+                'users.*.password' => 'required',
+            ];
+
+            $customMessages = [
+                'users.*.name.required' => 'Name field is required.',
+                'users.*.email.email' => 'Email field is required.',
+                'users.*.password.required' => ' Password  field is required.'
+            ];
+
+            $validator = Validator::make($data, $rules, $customMessages);
+            if ($validator->fails()) {
+                return response()->json($validator->errors(), 422);
+            }
+            foreach ($data['users'] as $addUser) {
+                $user = new User;
+                $user->name = $addUser['name'];
+                $user->email = $addUser['email'];
+                $user->password = bcrypt($addUser['password']);
+                $user->save();
+                $message = 'user successfully Added';
+                return response()->json(['message' => $message], 201);
+            }
         }
     }
 }
--- Laravel_Api/routes/api.php
+++ Laravel_Api/routes/api.php
@@ -20,3 +20,4 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 });
 Route::get('/users/{id?}', [UserApiController::class, 'ShowUser']);
 Route::post('/add-users', [UserApiController::class, 'AddUser']);
+Route::post('/add-multiple-users', [UserApiController::class, 'AddMultipleUser']);
